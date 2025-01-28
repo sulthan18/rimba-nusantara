15ea57a21ccaf447d5d5a7b7d56b93e9512658fc
sulthan rafi hendriansyah <sulthan19@gmail.com>
2025-01-29 00:43:48 +0700

    feat(config): add MongoDB configuration

--- app/api/v1/categories/router.js
+++ app/api/v1/categories/router.js
@@ -1,10 +1,14 @@
 const express = require("express");
+const { index, create, find, update, destroy } = require("./controller");
 const router = express();
 
-router.get("/categories", (req, res) => {
-  res.status(200).json({
-    message: "Halaman Categories",
-  });
-});
+router.get("/categories", index);
+router.get("/categories/:id", find);
+
+router.put("/categories/:id", update);
+
+router.delete("/categories/:id", destroy);
+
+router.post("/categories", create);
 
 module.exports = router;
